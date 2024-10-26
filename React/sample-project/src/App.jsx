01455b06ba4a2cd04096d58fb254f86e17e430d9
RavalisaiEnugandula <ravali.jntuhcej@gmail.com>
2024-10-26 17:31:53 +0530

    day 5

---
+++ React/sample-project/src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar"
+
+const App = () => {
+    const data = {
+        appname:"Hello"
+    }
+    const logodata = "mylogo"
+    const test= "hello world  "
+  return (
+    <>
+    <p className='text-red-500'>{test}</p>
+          <Navbar appdata={data} logo={logodata}/>
+    </>
+    
+  )
+}
+
+export default App
