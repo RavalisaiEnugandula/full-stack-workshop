75659d06d9462dd4d084c9653e8bb2124daea1f9
RavalisaiEnugandula <ravali.jntuhcej@gmail.com>
2024-11-14 17:02:53 +0530

    day 16

--- Express/server-mongoose/routes/AuthRoute.js
+++ Express/server-mongoose/routes/AuthRoute.js
@@ -7,25 +7,25 @@ router.post('/register', async (req, res) => {
     try {
         // const newuser = new Users(req.body)
         const { name, email, phone, password } = req.body
-        if (!name || !email || !phone || !password ) {
+        if (!name || !email || !phone || !password) {
             return res.status(401).json({ message: "All fields required" })
         }
 
         //TODO : Add User Email & Phone Validation
 
         //Email
-        const exisitingemail = await Users.findOne({email})
-        if(exisitingemail) {
-            return res.status(500).json({ message: `User with ${email} already exists !` })
+        const exisitingemail = await Users.findOne({ email })
+        if (exisitingemail) {
+            return res.status(409).json({ message: `User with ${email} already exists !` })
         }
 
         //Phone
-        const exisitingphone = await Users.findOne({phone})
-        if(exisitingphone) {
-            return res.status(500).json({ message: `User with ${phone} already exists !` })
+        const exisitingphone = await Users.findOne({ phone })
+        if (exisitingphone) {
+            return res.status(409).json({ message: `User with ${phone} already exists !` })
         }
         const salt = await bcrypt.genSalt(10)
-        const hashedpassword = await bcrypt.hash(password,salt)
+        const hashedpassword = await bcrypt.hash(password, salt)
         const newuser = new Users({
             name,
             email,
@@ -43,7 +43,7 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
     try {
         // const newuser = new Users(req.body)
-        const {  email, password } = req.body
+        const { email, password } = req.body
         if (!email || !password) {
             return res.status(401).json({ message: "All fields required" })
         }
@@ -51,19 +51,19 @@ router.post('/login', async (req, res) => {
         //TODO : Add User Email & Phone Validation
 
         //Email
-        const user = await Users.findOne({email})
-        if(!user) {
+        const user = await Users.findOne({ email })
+        if (!user) {
             return res.status(500).json({ message: `Invalid Email` })
         }
-        const checkpassword = await bcrypt.compare(password,user.password)
-        if(!checkpassword){
+        const checkpassword = await bcrypt.compare(password, user.password)
+        if (!checkpassword) {
             return res.status(500).json({ message: `Invalid Password` })
         }
-        
+
         //Generate JWT
-        const secretkey = '1234'
-        const token = jwt.sign({ email: email,role:user.role, exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 7)}, secretkey)
-        return res.status(200).json({ message: "login success",token: token })
+        const secretkey = '1811321'
+        const token = jwt.sign({ email: email, role: user.role, exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 7) }, secretkey)
+        return res.status(200).json({ message: "login success", token: token })
     } catch (error) {
         return res.status(500).json({ message: error.message })
     }
